815554d2726a78112feb99964b178dfef7323a97
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-04-10 11:19:44 -0400

    fianlizada questao

---
+++ questao5.js
@@ -0,0 +1,16 @@
+/*
+
+Questão 5: Crie uma matriz 3x3 e imprima o elemento na terceira linha e segunda
+coluna em JavaScript?
+
+*/
+reposta
+let matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+
+]
+
+console.log([2][1]);
+
