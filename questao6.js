3707f9971bf7ca9f4c7362c7dd1eaef13503711d
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-04-10 11:19:50 -0400

    fianlizada questao

---
+++ questao6.js
@@ -0,0 +1,19 @@
+/*
+
+Questão 6: Escreva um código para calcular a soma de todos os elementos de uma
+matriz em JavaScript.
+*/
+resposta
+let matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+
+]
+
+for(let i = 0; i <matriz.length; i++){
+    for(let j = 0;j < matriz[i].length; j++){
+     console.log(matriz[i][j]);
+    }
+    
+}
