5f2917df9dfdd3549132b4625253481a594652a4
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-04-10 11:19:29 -0400

    fianlizada questao

---
+++ questao2.js
@@ -0,0 +1,14 @@
+/*
+
+Questão 2: Crie um código em JavaScript para adicionar um elemento no final de
+um vetor.
+
+*/
+resposta
+let meuArray = [];
+
+meuArray.push(10);
+meuArray.push(20);
+meuArray.push(30);
+
+console.log(meuArray);
